0499c2be6265a5e7344993d05a983a4f4b41b916
Audumbar Nevarekar <audumbar@symboinsurance.com>
2020-09-29 12:24:43 +0530

    sql for driver

---
+++ driver.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: select * from driver
+LIMIT 0, 50000
+
+-- Date: 2020-09-29 10:21
+*/
+INSERT INTO `driver` (`id`,`email`,`first_name`,`last_name`,`middle_name`,`mobile`,`address`,`lat`,`lon`,`status`) VALUES (1,'aniket@gmail.com','Aniket','S',NULL,'9987654321','Navi Mumbai',19.158514699999998,72.99940185331164,'AVAILABLE');
+INSERT INTO `driver` (`id`,`email`,`first_name`,`last_name`,`middle_name`,`mobile`,`address`,`lat`,`lon`,`status`) VALUES (2,'audumbar@gmail.com','Audumbar','N',NULL,'8605219368','4B Ghodbunder Road, Thane',19.262750150000002,72.96728418250154,'AVAILABLE');
+INSERT INTO `driver` (`id`,`email`,`first_name`,`last_name`,`middle_name`,`mobile`,`address`,`lat`,`lon`,`status`) VALUES (3,'himanshu@gmail.com','Himanshu','S',NULL,'8605219369','Shri Kotilingeshwar Marg, Thane',19.18446325,72.97067817490179,'AVAILABLE');
+INSERT INTO `driver` (`id`,`email`,`first_name`,`last_name`,`middle_name`,`mobile`,`address`,`lat`,`lon`,`status`) VALUES (9,'abc@gmail.com','Sachin','T',NULL,'8605219370',NULL,19.38446325,72.97067817490179,'AVAILABLE');
+INSERT INTO `driver` (`id`,`email`,`first_name`,`last_name`,`middle_name`,`mobile`,`address`,`lat`,`lon`,`status`) VALUES (10,'abcd@gmail.com','Aman','A',NULL,'8605219371',NULL,19.48446325,72.87067817490178,'AVAILABLE');
